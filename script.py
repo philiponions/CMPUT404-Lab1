3c8d503add224a4d3d0c59f22cd32e73fab85221
philiponions <hueycoy9@gmail.com>
2023-09-11 17:47:00 -0600

    grab python script from raw url

--- script.py
+++ script.py
@@ -2,5 +2,5 @@ import requests
 
 print(requests.__version__)
 
-r = requests.get("http://google.com")
-print(r)
+r = requests.get("https://raw.githubusercontent.com/philiponions/CMPUT404-Lab1/main/script.py")
+print(r.text)
